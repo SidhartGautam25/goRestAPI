255047eb410a99920f7b9e706be79ccf5e2e57c3
sidhart25 <navneetnishchal1420@gmail.com>
2025-01-10 19:49:08 +0530

    server started successfully

---
+++ cmd/goRestAPI/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/SidhartGautam25/goRestAPI/internal/config"
+)
+
+func main() {
+	// load config
+	cfg := config.MustLoad()
+	// databse setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to student api"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGABRT)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("error,server not started")
+
+		}
+
+	}()
+	<-done
+
+	slog.Info("shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+
+}
